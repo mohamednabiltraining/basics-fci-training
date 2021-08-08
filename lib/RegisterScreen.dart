2dcd50d3253897f77fdd1489d53f38052deee77c
Mohamed Nabil <m.nabil.fci2015@gmail.com>
2021-08-08 20:35:01 +0200

    added register screen configured main route

---
+++ lib/RegisterScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  static const routeName = 'Register';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+    );
+  }
+}
